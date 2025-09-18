43c5b71ebcdd32bc3e649b133bc37bcc97d017e3
KrasnosheevMikhail <mkrasn@yandex.com>
2025-09-18 18:09:59 +0300

    Update Food.java

--- src/Food.java
+++ src/Food.java
@@ -2,5 +2,3 @@ public interface Food {
     void eat();
 }
 //Interface Segregation Principle - применяем разные интерфейсы вместо одного большого
-//Dependency Inversion Principle - разделив интерфейсы мы не зависим от их общей функциональности,
-// мы можем поменять один интерфейс на другой
