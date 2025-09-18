28b9780ac78723383ba261f875330e0075eb3326
KrasnosheevMikhail <mkrasn@yandex.com>
2025-09-18 22:41:08 +0300

    Update Device.java

--- src/Device.java
+++ src/Device.java
@@ -1,5 +1,5 @@
 public interface Device {
     void working();
 }
-//Dependency Inversion Principle - разделив интерфейсы мы не зависим от их общей функциональности,
-// мы можем поменять один интерфейс на другой
+
+
